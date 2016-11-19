cec1746ace1f864ef61f8b4821a5f4c979d2ecbc
Yohannes Wijaya <readyforservice@icloud.com>
2016-11-19 14:13:28 +0700

    Added & setup detail table view controller

---
+++ Pokedex/DetailPokedexTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailPokedexTableViewController.swift
+//  Pokedex
+//
+//  Created by Yohannes Wijaya on 11/19/16.
+//  Copyright © 2016 Yohannes Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class DetailPokedexTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
