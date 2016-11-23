908c323aaa0e7e86a4c6173ecfab07a75989200f
Yohannes Wijaya <readyforservice@icloud.com>
2016-11-23 11:42:23 +0700

    Display characters in Favorites' tab.

--- Pokedex/FavoritesCollectionViewController.swift
+++ Pokedex/FavoritesCollectionViewController.swift
@@ -10,9 +10,41 @@ import UIKit
 
 class FavoritesCollectionViewController: UICollectionViewController {
 
+    // MARK: - Stored Properties
+    
+    var favorites = [String]()
+    
+    // MARK: - UIViewController Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.title = "Favorites"
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let userdefaults = UserDefaults.standard
+        self.favorites = userdefaults.object(forKey: "Favorites") as? [String] ?? [String]()
+        
+        self.collectionView?.reloadData()
+    }
+    
+    // MARK: - UICollectionViewDataSource Methods
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.favorites.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ReusableCell", for: indexPath)
+        
+        if let validImageView = cell.viewWithTag(123) as? UIImageView {
+            let pokemon = self.favorites[indexPath.item]
+            validImageView.image = UIImage(named: pokemon)
+        }
+        
+        return cell
     }
 }
